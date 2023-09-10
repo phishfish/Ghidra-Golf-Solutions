44c0ff11856ad519003d508eefa2f40dfab0d70c
phishfish <lucialwang@gmail.com>
2023-09-09 23:56:17 -0400

    Python solurion

--- Challenge-2/Metadata.py
+++ Challenge-2/Metadata.py
@@ -1,7 +1,9 @@
 def run():
     # find the missing function. replace CHANGEME
-    println(getCurrentProgram().CHANGEME()) # get executable path
+    println(getCurrentProgram().getExecutablePath()) # get executable path
 
-    println(getCurrentProgram().CHANGEME()) # get name
+    println(getCurrentProgram().getName()) # get name
 
-    println(getCurrentProgram().CHANGEME()) # get md5 hash
+    println(getCurrentProgram().getExecutableMD5()) # get md5 hash
+    
+run()
